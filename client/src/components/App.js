7fe02d69a27635293c069657b7dafb1dd81acfb6
Masoud <masoud_mdar@yahoo.fr>
2021-08-31 19:30:11 +0200

    basics

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import axios from "axios"
+
+const App = () => {
+
+    return (
+        <div className="container">
+            Hello World !
+        </div>
+    )
+}
+
+export default App
