6cba455e7c43601b8f342c4b6a858ad51d693d3c
Masoud <masoud_mdar@yahoo.fr>
2021-08-31 19:47:38 +0200

    main page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,41 @@
-import React from "react"
+import React, {useState} from "react"
 
 import axios from "axios"
+import LoadPage from "./LoadPage"
 
 const App = () => {
 
+    const [isLoading, setIsLoading] = useState(false)
+    const [isMainPage, setIsMainPage] = useState(true)
+
+    const handleClick = (Event) => {
+
+    }
+
+
+
     return (
-        <div className="container">
-            Hello World !
+        <div>
+
+            <div className="container">
+                {
+                    isLoading ? (
+
+                        <LoadPage />
+
+                    ) : (
+
+                        <div className="main-page">
+                            <button name="book-list" onClick={handleClick} className="btn">Book List</button>
+                            <button name="panier" onClick={handleClick} className="btn">Panier</button>
+                        </div>
+
+                    )
+                }
+            </div>
+
         </div>
+
     )
 }
 
---
+++ client/src/components/LoadPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const LoadPage = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default LoadPage
