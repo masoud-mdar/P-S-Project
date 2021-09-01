1b0eb82f7b1b4c2acb499da035d8ff80ca0da9e5
Masoud <masoud_mdar@yahoo.fr>
2021-09-01 10:49:23 +0200

    main page set

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,9 +7,34 @@ const App = () => {
 
     const [isLoading, setIsLoading] = useState(false)
     const [isMainPage, setIsMainPage] = useState(true)
+    const [isListPage, setIsListPage] = useState(false)
+    const [isPanier, setIsPanier] = useState(false)
 
     const handleClick = (Event) => {
+        const {name} = Event.target
 
+        switch (name) {
+            case "book-list" :
+                console.log("yessss")
+                setIsListPage(true)
+                setIsMainPage(false)
+                setIsPanier(false)
+                break
+
+            case "panier" :
+                setIsPanier(true)
+                setIsMainPage(false)
+                setIsListPage(false)
+
+            case "fetch" :
+                fetch("https://henri-potier.techx.fr/books")
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                    })
+            default :
+                console.log(name)
+        }
     }
 
 
@@ -17,7 +42,7 @@ const App = () => {
     return (
         <div>
 
-            <div className="container">
+            <div>
                 {
                     isLoading ? (
 
@@ -25,11 +50,34 @@ const App = () => {
 
                     ) : (
 
-                        <div className="main-page">
-                            <button name="book-list" onClick={handleClick} className="btn">Book List</button>
-                            <button name="panier" onClick={handleClick} className="btn">Panier</button>
+                        <div className="container">
+
+                            <div className="nav-bar"></div>
+
+                            {
+                                isMainPage ? (
+
+                                    <div className="main-page">
+                                        <div className="btn-wrapper">
+                                            <button name="book-list" onClick={handleClick} className="btn">Book List</button>
+                                            <button name="panier" onClick={handleClick} className="btn">Panier</button>
+                                            {/*<button name="fetch" onClick={handleClick} className="btn">Fetch Data</button>*/}
+                                        </div>
+                                        <div className="smile"></div>
+
+                                    </div>      
+
+                                ) : isListPage ? (
+
+                                    <div className="list-page"></div>
+
+                                ) : (
+                                    <div className="panier"></div>
+                                )
+                            }
                         </div>
 
+                        
                     )
                 }
             </div>
