3cbb964ad932dc034cefc8e8ad217dede61c9fff
Masoud <masoud_mdar@yahoo.fr>
2021-09-02 15:33:20 +0200

    seperating styles

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import App from "./components/App"
+
 import "./styles/styles.css"
+import "./styles/mainPage.css"
+import "./styles/navBar.css"
+import "./styles/listPage.css"
+import "./styles/moreInfo.css"
+import "./styles/panier.css"
+import "./styles/popUp.css"
+import "./styles/footer.css"
 
 ReactDOM.render(<App />, document.getElementById("root"))
